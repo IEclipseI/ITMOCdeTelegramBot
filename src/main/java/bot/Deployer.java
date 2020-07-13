27f4e317c6419a4c48c6aac0a3e65c441a55a078
Roman Smirnov <smirnovromal@yandex.ru>
2020-07-14 02:21:29 +0300

    log props

--- src/main/java/bot/Deployer.java
+++ src/main/java/bot/Deployer.java
@@ -1,12 +1,16 @@
 package bot;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Deployer {
+    private static final Logger logger = LogManager.getLogger(Rosmira.class);
+
     public static void main(String[] args) {
-        System.out.println("Maven works for Rosmira!!!");
+        logger.info("Maven works for Rosmira!!!");
 
         ApiContextInitializer.init();
 
