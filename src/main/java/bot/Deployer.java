80f9cc6035105bd7c03e1fe73c488588925e73c4
Roman Smirnov <smirnovromal@yandex.ru>
2020-07-14 13:21:18 +0300

    socket

--- src/main/java/bot/Deployer.java
+++ src/main/java/bot/Deployer.java
@@ -6,11 +6,16 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Deployer {
     private static final Logger logger = LogManager.getLogger(Rosmira.class);
 
     public static void main(String[] args) {
         logger.info("Maven works for Rosmira!!!");
+        System.out.println("println qwerty!!!");
 
         ApiContextInitializer.init();
 
@@ -19,6 +24,14 @@ public class Deployer {
         try {
             botsApi.registerBot(new Rosmira());
         } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+        try (ServerSocket serverSocket = new ServerSocket(Integer.valueOf(System.getenv("PORT")))) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
