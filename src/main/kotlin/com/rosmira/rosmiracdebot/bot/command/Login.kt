33656f7fc1b91f269af52ac5e7bb1396cd66a7a1
Roman Smirnov <smirnovromal@yandex.ru>
2020-07-28 19:23:16 +0300

    fun

--- src/main/kotlin/com/rosmira/rosmiracdebot/bot/command/Login.kt
+++ src/main/kotlin/com/rosmira/rosmiracdebot/bot/command/Login.kt
@@ -6,8 +6,9 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Chat
 import org.telegram.telegrambots.meta.api.objects.User
 import org.telegram.telegrambots.meta.bots.AbsSender
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException
 
-class Login: BotCommand("login", "<login in CDE> <password>"), Logging {
+class Login : BotCommand("login", "<login in CDE> <password>"), Logging {
     override fun execute(absSender: AbsSender, user: User, chat: Chat, strings: Array<String>) {
         val stringBuilder = StringBuilder()
         for ((index, value) in strings.withIndex()) {
@@ -15,7 +16,21 @@ class Login: BotCommand("login", "<login in CDE> <password>"), Logging {
         }
         logger.info("Received login request: $stringBuilder")
 
-        val msg = SendMessage().setChatId(chat.id).setText("You are logined")
-        absSender.execute(msg)
+        try {
+            if (strings.size < 2) {
+                val notEnoughArgs = SendMessage()
+                notEnoughArgs.text =
+                        """Command requires two arguments:
+                            |/login LOGIN PASSWORD
+                        """.trimMargin()
+                notEnoughArgs.setChatId(chat.id)
+                absSender.execute(notEnoughArgs)
+            } else {
+                val msg = SendMessage().setChatId(chat.id).setText("You are logined")
+                absSender.execute(msg)
+            }
+        } catch (e: TelegramApiException) {
+            logger.error("Cannot send response: ${e.stackTrace}")
+        }
     }
 }
